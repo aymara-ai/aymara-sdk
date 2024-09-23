b5db62cc2fe67a04ad9da742e5b5cb2588cace40
jm-contreras <jm.contreras.phd@gmail.com>
2024-09-23 13:04:24 -0400

    Update the safety docs and make them leaner.

--- aymara_sdk/examples/demo_student.py
+++ aymara_sdk/examples/demo_student.py
@@ -1,17 +1,19 @@
 """Students are the models that take tests."""
 
 import os
-
+import asyncio
 from openai import OpenAI
 
+from aymara_sdk.types import StudentAnswerInput
 
 class OpenAIStudent:
     """OpenAI API student."""
 
-    def __init__(self, model="gpt-4o-mini", api_args=None):
+    def __init__(self, model="gpt-4o-mini", api_key=None):
         self.model = model
-        self.api_args = api_args
-        self.client = OpenAI(api_key=os.environ.get("OPENAI_KEY"))
+        if api_key is None:
+            api_key=os.environ.get("OPENAI_KEY")
+        self.client = OpenAI(api_key=api_key)
 
     def answer_question(self, question: str) -> str:
         """Answer a test question."""
@@ -21,3 +23,20 @@ class OpenAIStudent:
         )
 
         return completion.choices[0].message.content
+
+    async def get_student_answer(self, question):
+        answer_text = await asyncio.to_thread(self.answer_question, question.question_text)
+        return StudentAnswerInput(question_uuid=question.question_uuid, answer_text=answer_text)
+
+    async def get_all_student_answers(self, questions):
+        return await asyncio.gather(*[self.get_student_answer(question) for question in questions])
+
+    async def process_tests(self, tests):
+        all_student_answers = await asyncio.gather(*[self.get_all_student_answers(test.questions) for test in tests])
+        
+        student_answers_dict = {}
+        for test, student_answers in zip(tests, all_student_answers):
+            student_answers_dict[test.test_uuid] = student_answers
+        
+        return student_answers_dict
+    
