a076272fe7ff7396ec3bd8961459edaaa76554af
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2024-10-03 14:56:18 -0700

    fix constant

--- aymara_sdk/utils/constants.py
+++ aymara_sdk/utils/constants.py
@@ -4,7 +4,7 @@ from typing import List
 POLLING_INTERVAL = 1  # seconds
 
 # Test Creation Defaults
-DEAFULT_MAX_WAIT_TIME_SECS: int = 120
+DEFAULT_MAX_WAIT_TIME_SECS: int = 120
 DEFAULT_NUM_QUESTIONS: int = 20
 DEFAULT_TEST_LANGUAGE: str = "en"
 
