dfdb6ab3cafff7f7398f5979a10b700e3706d798
dr3s <dr3s@users.noreply.github.com>
2025-04-21 15:45:55 -0400

    support accuracy in create_test (#47)

--- tests/integration/test_tests_integration.py
+++ tests/integration/test_tests_integration.py
@@ -637,6 +637,19 @@ class TestTestMixin:
                 questions_by_type[question.accuracy_question_type] = []
             questions_by_type[question.accuracy_question_type].append(question)
 
+    def test_create_accuracy_test_create_test_fn(self, aymara_client, accuracy_test_data):
+        accuracy_test_data["test_type"] = TestType.ACCURACY
+        response = aymara_client.create_test(**accuracy_test_data)
+        assert isinstance(response, AccuracyTestResponse)
+        assert response.test_status == Status.COMPLETED
+
+        # Group questions by type
+        questions_by_type = {}
+        for question in response.questions:
+            if question.accuracy_question_type not in questions_by_type:
+                questions_by_type[question.accuracy_question_type] = []
+            questions_by_type[question.accuracy_question_type].append(question)
+
     async def test_create_accuracy_test_async_with_examples(self, aymara_client, accuracy_test_data):
         response = await aymara_client.create_accuracy_test_async(**accuracy_test_data)
         assert isinstance(response, AccuracyTestResponse)
